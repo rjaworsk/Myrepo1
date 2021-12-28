0973fee90ee415a6ea084208f8e62a6112e45a62
rjaworsk <92691704+rjaworsk@users.noreply.github.com>
2021-12-28 19:09:46 +0100

    changes in main file for master

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ package Task1;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Code at line 4"); 
+                 System.out.prtinln("Code at line 5  are also added in Master branch"); 
 	}
 
 }
